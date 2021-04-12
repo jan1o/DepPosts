1db151b399a570ebe657c72edcb74ca32ad892a6
Janio Fernandes <juniorjanio2016@gmail.com>
2021-04-12 12:59:39 -0300

    Inicio concerto do model posts

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from revistas.models import Revista
-
+from usuarios.models import Usuario
 
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
     corpo = models.CharField(max_length=1000)
     dataPublicacao = models.DateTimeField(auto_now_add=True)
-    revistaAssociada = models.OneToOneField(Revista, on_delete=models.CASCADE, default='')
+    revistaAssociada = models.ForeignKey(Revista, on_delete=models.CASCADE, default='', null=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, default='', null=True)
 
     def __str__(self):
         return self.titulo
